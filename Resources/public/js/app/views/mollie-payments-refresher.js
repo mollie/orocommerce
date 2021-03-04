a7bb135c3fad54a349726b8a464b75c9113d0292
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-03-04 12:36:53 +0100

    Create new JS component for fetching enabled methods
    Add message on website profile change
    
    ISSUE: MOCRSET05-63 & MOCRSET05-64

--- Resources/public/js/app/views/mollie-payments-refresher.js
+++ Resources/public/js/app/views/mollie-payments-refresher.js
@@ -16,6 +16,7 @@ define([
             this.websiteChooserSelector = options.websiteChooserSelector;
             this.websiteProfileChangeMarkerName = options.websiteProfileChangeMarkerName;
             this.formSaveMarkerSelector = options.formSaveMarkerSelector;
+            this.backendUrl = options.backendUrl;
 
             $(this.websiteChooserSelector).change(_.bind(this.onWebsiteChange, this));
 
@@ -53,6 +54,30 @@ define([
                     url: url, type: $form.attr('method'), data: $.param(data),
                     complete: _.bind(this.onPageSubmitComplete, this)
                 });
+            }
+
+            this.getEnabledMethods($(this.websiteChooserSelector).val());
+        },
+
+        getEnabledMethods: function(selectedProfile) {
+            let url = this.backendUrl.replace('mollieProfileId', selectedProfile);
+            console.log(url);
+            $.ajax({
+                url: url,
+                type: 'GET',
+                success: this.successHandler.bind(this),
+            });
+        },
+
+        /**
+         * @param {{success: boolean, activeMethods: string}} response
+         */
+        successHandler: function(response) {
+            let activeMethods = $('.mollie-enabled-methods');
+            let activeMethodsWrapper = $('.mollie-enabled-methods-wrapper');
+            if (activeMethods && response.success && activeMethodsWrapper) {
+                activeMethods.text(response.activeMethods);
+                activeMethodsWrapper.removeClass('mollie-hidden');
             }
         },
 
