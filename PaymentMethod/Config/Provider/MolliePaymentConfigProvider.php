03e008b5056f887a930978b05671c8967d1b7fd2
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-03-09 15:55:54 +0100

    Adjust to ORO4
    ISSUE: MOCRSET05-46 && MOCRSET05-47

--- PaymentMethod/Config/Provider/MolliePaymentConfigProvider.php
+++ PaymentMethod/Config/Provider/MolliePaymentConfigProvider.php
@@ -18,7 +18,7 @@ use Mollie\Bundle\PaymentBundle\PaymentMethod\Config\Factory\PaymentConfigFactor
 use Mollie\Bundle\PaymentBundle\PaymentMethod\Config\MolliePaymentConfigInterface;
 use Oro\Bundle\LocaleBundle\Entity\LocalizedFallbackValue;
 use Oro\Bundle\PaymentBundle\Method\Config\PaymentConfigInterface;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class MolliePaymentConfigProvider
