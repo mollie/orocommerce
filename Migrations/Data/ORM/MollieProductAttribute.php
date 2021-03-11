67540c5dd5d8f206a6ae6ce14787f1b31882a465
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-03-11 09:42:03 +0100

    Add Mollie Voucher Category migration script
    ISSUE: MOCRSET05-33

--- Migrations/Data/ORM/MollieProductAttribute.php
+++ Migrations/Data/ORM/MollieProductAttribute.php
@@ -1,31 +1,110 @@
 <?php
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Mollie\Bundle\PaymentBundle\IntegrationCore\BusinessLogic\PaymentMethod\Model\PaymentMethodConfig;
+use Oro\Bundle\EntityConfigBundle\Attribute\Entity\AttributeFamily;
+use Oro\Bundle\EntityConfigBundle\Attribute\Entity\AttributeGroupRelation;
+use Oro\Bundle\EntityExtendBundle\Entity\Repository\EnumValueRepository;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
-use Oro\Bundle\LocaleBundle\Migrations\Data\ORM\LoadLocalizationData;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\ProductBundle\Entity\Product;
+use Oro\Bundle\ProductBundle\Migrations\Data\ORM\LoadProductDefaultAttributeFamilyData;
+use Oro\Bundle\ProductBundle\Migrations\Data\ORM\MakeProductAttributesTrait;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-class MollieProductAttribute extends AbstractFixture implements
-    ContainerAwareInterface,
-    DependentFixtureInterface
+/**
+ * Class MollieProductAttribute
+ */
+class MollieProductAttribute extends AbstractFixture implements ContainerAwareInterface
 {
-    use Oro\Bundle\ProductBundle\Migrations\Data\ORM\MakeProductAttributesTrait;
+    use MakeProductAttributesTrait;
 
-    public function load(\Doctrine\Common\Persistence\ObjectManager $objectManager)
+    /**
+     * @var array[]
+     */
+    private static $selectOptions = [
+        PaymentMethodConfig::VOUCHER_CATEGORY_NONE => [
+            'name' => 'None',
+            'is_default' => true,
+        ],
+        PaymentMethodConfig::VOUCHER_CATEGORY_GIFT => [
+            'name' => 'Gift',
+            'is_default' => false,
+        ],
+        PaymentMethodConfig::VOUCHER_CATEGORY_ECO => [
+            'name' => 'Eco',
+            'is_default' => false,
+        ],
+        PaymentMethodConfig::VOUCHER_CATEGORY_MEAL => [
+            'name' => 'Meal',
+            'is_default' => false,
+        ],
+    ];
+
+    /**
+     * @inheritdoc
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
     {
+        $this->addEnumOptions($manager);
+
         $this->makeProductAttributes(
             [
-                'voucher_category' => []
+                \Mollie\Bundle\PaymentBundle\Migrations\Schema\v1_1\MollieProductAttribute::VOUCHER_CATEGORY_FIELD_NAME => []
             ],
-            ExtendScope::OWNER_SYSTEM
+            ExtendScope::OWNER_CUSTOM
         );
+
+        $this->setDefaultFamily($manager);
     }
 
-    public function getDependencies()
+    /**
+     * Add mollie voucher category options
+     *
+     * @param ObjectManager $manager
+     */
+    private function addEnumOptions(ObjectManager $manager)
     {
-        return [
-            LoadLocalizationData::class
-        ];
+        $className = ExtendHelper::buildEnumValueClassName(\Mollie\Bundle\PaymentBundle\Migrations\Schema\v1_1\MollieProductAttribute::VOUCHER_CATEGORY_FIELD_CODE);
+
+        /** @var EnumValueRepository $enumRepo */
+        $enumRepo = $manager->getRepository($className);
+
+        $priority = 1;
+        foreach (static::$selectOptions as $id => $option) {
+            $enumOption = $enumRepo->createEnumValue($option['name'], $priority++, $option['is_default']);
+            $manager->persist($enumOption);
+        }
+    }
+
+    /**
+     * Set default family for mollie voucher category
+     *
+     * @param ObjectManager $manager
+     */
+    private function setDefaultFamily(ObjectManager $manager)
+    {
+        $defaultFamily = $manager->getRepository(AttributeFamily::class)
+            ->findOneBy(['code' => LoadProductDefaultAttributeFamilyData::DEFAULT_FAMILY_CODE]);
+
+        $this->setReference(LoadProductDefaultAttributeFamilyData::DEFAULT_FAMILY_CODE, $defaultFamily);
+
+        $attributeGroup = $defaultFamily->getAttributeGroup(LoadProductDefaultAttributeFamilyData::GENERAL_GROUP_CODE);
+
+        $configManager = $this->getConfigManager();
+        $variantField = $configManager->getConfigFieldModel(
+            Product::class,
+            \Mollie\Bundle\PaymentBundle\Migrations\Schema\v1_1\MollieProductAttribute::VOUCHER_CATEGORY_FIELD_NAME
+        );
+
+        $attributeGroupRelation = new AttributeGroupRelation();
+        if ($variantField) {
+            $attributeGroupRelation->setEntityConfigFieldId($variantField->getId());
+            $attributeGroup->addAttributeRelation($attributeGroupRelation);
+            $manager->persist($defaultFamily);
+            $manager->flush();
+        }
     }
 }
--- Migrations/Schema/v1_1/MollieProductAttribute.php
+++ Migrations/Schema/v1_1/MollieProductAttribute.php
@@ -15,39 +15,58 @@ class MollieProductAttribute implements Migration, ExtendExtensionAwareInterface
 {
 
     const PRODUCT_TABLE_NAME = 'oro_product';
+    const VOUCHER_CATEGORY_FIELD_NAME = 'mollie_voucher_category';
+    const VOUCHER_CATEGORY_FIELD_CODE = 'mollie_voucher';
 
     /**
      * @var ExtendExtension
      */
     private $extendExtension;
 
+    /**
+     * @inheritdoc
+     * @param Schema $schema
+     * @param QueryBag $queries
+     *
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
     public function up(Schema $schema, QueryBag $queries)
     {
         $this->addMollieProductAttribute($schema);
     }
 
+    /**
+     * @inheritdoc
+     * @param ExtendExtension $extendExtension
+     */
     public function setExtendExtension(ExtendExtension $extendExtension)
     {
         $this->extendExtension = $extendExtension;
     }
 
+    /**
+     * Adds Mollie Voucher column
+     *
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
     protected function addMollieProductAttribute(Schema $schema)
     {
         if ($schema->hasTable(self::PRODUCT_TABLE_NAME)) {
-            $table = $schema->getTable(self::PRODUCT_TABLE_NAME);
-            if ($table->hasColumn('voucher_category_id')) {
-                return;
-            }
+            $table = $schema->getTable('oro_product');
 
             $this->extendExtension->addEnumField(
                 $schema,
                 $table,
-                'voucher_category',
-                'voucher_category',
+                self::VOUCHER_CATEGORY_FIELD_NAME,
+                self::VOUCHER_CATEGORY_FIELD_CODE,
                 false,
                 false,
                 [
                     'extend' => ['owner' => ExtendScope::OWNER_CUSTOM],
+                    'entity' => ['label' => 'mollie.payment.extend.entity.voucher_category.label'],
+                    'attribute' => ['is_attribute' => true, 'searchable' => true, 'filterable' => true],
                     'importexport' => ['excluded' => true]
                 ]
             );
