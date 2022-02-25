22b6ff7a032c6e475ca03fc268414885b1b10ada
Tamara <tamara@example.com>
2022-02-25 10:11:43 +0100

    Change surcharge calculation
    MOCRSET10-25

--- PaymentMethod/View/MolliePaymentView.php
+++ PaymentMethod/View/MolliePaymentView.php
@@ -2,6 +2,7 @@
 
 namespace Mollie\Bundle\PaymentBundle\PaymentMethod\View;
 
+use Mollie\Bundle\PaymentBundle\IntegrationCore\BusinessLogic\Surcharge\SurchargeService;
 use Mollie\Bundle\PaymentBundle\PaymentMethod\Config\MolliePaymentConfigInterface;
 use Oro\Bundle\PaymentBundle\Context\PaymentContextInterface;
 use Oro\Bundle\PaymentBundle\Method\Provider\PaymentMethodProvider;
@@ -54,10 +55,21 @@ class MolliePaymentView implements PaymentMethodViewInterface
             }
         }
 
+        $surchargeType = $this->config->getSurchargeType();
+        $surchargeFixedAmount = $this->config->getSurchargeFixedAmount();
+        $surchargePercentage = $this->config->getSurchargePercentage();
+        $surchargeLimit = $this->config->getSurchargeLimit();
+
         return [
             'isApplePay' => false !== strpos($this->config->getPaymentMethodIdentifier(), 'applepay'),
             'icon' => $this->config->getIcon(),
-            'surchargeAmount' => $this->config->getSurchargeAmount(),
+            'surchargeAmount' => $this->getSurchargeService()->calculateSurchargeAmount(
+                $surchargeType,
+                $surchargeFixedAmount,
+                $surchargePercentage,
+                $surchargeLimit,
+                $context->getSubtotal()->getValue()
+            ),
             'currency' => $context->getCurrency(),
             'useMollieComponents' => $this->config->useMollieComponents() && !$this->isMultipleCreditCard($context),
             'useSingleClickPayment' => $this->config->useMollieComponents() && !$this->isMultipleCreditCard($context) &&
@@ -74,6 +86,46 @@ class MolliePaymentView implements PaymentMethodViewInterface
             'renderSaveCreditCardCheckbox' => $renderSaveCreditCardCheckbox,
             'renderUseSavedCreditCardCheckbox' => $renderUseSavedCreditCardCheckbox,
         ];
+    }
+
+    /**
+     * @return CustomerReferenceService
+     */
+    protected function getCustomerReferenceService(): CustomerReferenceService
+    {
+        /** @var CustomerReferenceService $customerReferenceService */
+        $customerReferenceService = ServiceRegister::getService(CustomerReferenceService::CLASS_NAME);
+
+        return $customerReferenceService;
+    }
+
+    /**
+     * @return SurchargeService
+     */
+    protected function getSurchargeService()
+    {
+        /** @var SurchargeService $surchargeService */
+        $surchargeService = ServiceRegister::getService(SurchargeService::CLASS_NAME);
+
+        return $surchargeService;
+    }
+
+    /**
+     * @param PaymentContextInterface $context
+     *
+     * @return bool
+     */
+    private function isMultipleCreditCard(PaymentContextInterface $context)
+    {
+        $applicablePaymentMethods = $this->paymentMethodProvider->getApplicablePaymentMethods($context);
+        $numberOfMollieCreditCards = 0;
+        foreach ($applicablePaymentMethods as $key => $config) {
+            if (preg_match('/^mollie.*creditcard$/', $key)) {
+                $numberOfMollieCreditCards++;
+            }
+        }
+
+        return $numberOfMollieCreditCards > 1;
     }
 
     /**
@@ -112,34 +164,5 @@ class MolliePaymentView implements PaymentMethodViewInterface
     public function getPaymentMethodIdentifier()
     {
         return $this->config->getPaymentMethodIdentifier();
-    }
-
-    /**
-     * @return CustomerReferenceService
-     */
-    protected function getCustomerReferenceService(): CustomerReferenceService
-    {
-        /** @var CustomerReferenceService $customerReferenceService */
-        $customerReferenceService = ServiceRegister::getService(CustomerReferenceService::CLASS_NAME);
-
-        return $customerReferenceService;
-    }
-
-    /**
-     * @param PaymentContextInterface $context
-     *
-     * @return bool
-     */
-    private function isMultipleCreditCard(PaymentContextInterface $context)
-    {
-        $applicablePaymentMethods = $this->paymentMethodProvider->getApplicablePaymentMethods($context);
-        $numberOfMollieCreditCards = 0;
-        foreach ($applicablePaymentMethods as $key => $config) {
-            if (preg_match('/^mollie.*creditcard$/', $key)) {
-                $numberOfMollieCreditCards++;
-            }
-        }
-
-        return $numberOfMollieCreditCards > 1;
     }
 }
