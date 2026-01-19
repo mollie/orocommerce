fa93fc471c6afac530aa77dc5dde66570dfa4eec
Matija Stankovic <matija.stankovic@logeecom.com>
2026-01-19 10:06:39 +0100

    Add compatibility with OroCommerce 6.1
    ISSUE: MLFL-379

--- IntegrationServices/OrderTransitionService.php
+++ IntegrationServices/OrderTransitionService.php
@@ -9,6 +9,7 @@ use Mollie\Bundle\PaymentBundle\IntegrationCore\Infrastructure\Logger\Logger;
 use Mollie\Bundle\PaymentBundle\PaymentMethod\MolliePayment;
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
 use Oro\Bundle\EntityExtendBundle\Entity\AbstractEnumValue;
+use Oro\Bundle\EntityExtendBundle\Entity\EnumOption;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
 use Oro\Bundle\OrderBundle\Entity\Order as OroOrder;
 use Oro\Bundle\OrderBundle\Provider\OrderStatusesProviderInterface;
@@ -276,12 +277,13 @@ class OrderTransitionService implements OrderTransitionServiceInterface
      */
     protected function getInternalStatus($statusId)
     {
-        $className = ExtendHelper::buildEnumValueClassName(OroOrder::INTERNAL_STATUS_CODE);
-        $entityManager = $this->doctrineHelper->getEntityManagerForClass($className);
+        $entityManager = $this->doctrineHelper->getEntityManagerForClass(EnumOption::class);
         if (!$entityManager) {
             return null;
         }
 
-        return $entityManager->getRepository($className)->find($statusId);
+        $enumOptionId = ExtendHelper::buildEnumOptionId(OroOrder::INTERNAL_STATUS_CODE, $statusId);
+
+        return $entityManager->getRepository(EnumOption::class)->find($enumOptionId);
     }
 }
